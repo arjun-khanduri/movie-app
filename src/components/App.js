31a8dd24afb8b1488c65a4f43fe5ff68231b71c1
Arjun Khanduri <arjun99.khanduri@gmail.com>
2021-03-24 23:43:21 +0530

    Added two components

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,23 @@
-
+import React from 'react';
+import {data} from '../data';
+import Navbar from './Navbar';
 function App() {
   return (
     <div className="App">
-      Project setup
+      {/* <Navbar /> */}
+      <div className="main">
+        <div className="tabs">
+          Movies
+        </div>
+        <div className="tabs">
+          Favorites
+        </div>
+        <div className="list">
+          {data.map(movie => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
