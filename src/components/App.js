8d8d2f30e1a5307ec63cd1163dfbdedbc61acc82
Arjun Khanduri <arjun99.khanduri@gmail.com>
2021-03-31 21:50:18 +0530

    Added functionality to subscribe to the store

--- src/components/App.js
+++ src/components/App.js
@@ -2,24 +2,41 @@ import React from 'react';
 import { data } from '../data';
 import MovieCard from './MovieCard';
 import Navbar from './Navbar';
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <div className="main">
-        <div className="tabs">
-          <div className="tab">Movies</div>
-          <div className="tab">Favorites</div>
-        </div>
+class App extends React.Component {
+  
+  componentDidMount() {
+    const {store} = this.props;
+    store.subscribe(() => {
+      console.log('UPDATED');
+      this.forceUpdate();
+    });
 
-        <div className="list">
-          {data.map((movie, index) => (
-            <MovieCard movie={movie} key={`movies-${index}`} />
-          ))}
+    store.dispatch({
+      type: 'ADD_MOVIES',
+      movies: data
+    });
+    console.log('STATE', this.props.store.getState());
+  }
+
+  render() {
+    const movies = this.props.store.getState();
+    return (
+      <div className="App">
+        <Navbar />
+        <div className="main">
+          <div className="tabs">
+            <div className="tab">Movies</div>
+            <div className="tab">Favorites</div>
+          </div>
+          <div className="list">
+            {movies.map((movie, index) => (
+              <MovieCard movie={movie} key={`movies-${index}`} />
+            ))}
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import App from './components/App';
 import movies from './reducers';
 
 const store = createStore(movies);
-console.log('store', store.getState());
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<App store={store} />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
