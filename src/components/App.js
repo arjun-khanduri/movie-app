89ba9c616db71eee8e60d7bf187ef0b1bd620964
Arjun Khanduri <arjun99.khanduri@gmail.com>
2021-04-08 22:52:59 +0530

    Added search reducers

--- src/components/App.js
+++ src/components/App.js
@@ -18,8 +18,8 @@ class App extends React.Component {
   }
 
   isMovieFavorite = (movie) => {
-    const { favorites } = this.props.store.getState();
-    const index = favorites.indexOf(movie);
+    const { movies } = this.props.store.getState();
+    const index = movies.favorites.indexOf(movie);
     if (index !== -1)
       return true; //Movie found 
     return false;
@@ -30,7 +30,8 @@ class App extends React.Component {
   }
 
   render() {
-    const { list, favorites, showFavorites } = this.props.store.getState();
+    const { movies } = this.props.store.getState();
+    const { list, favorites, showFavorites } = movies;
     console.log('Rendered', this.props.store.getState());
     const displayMovies = showFavorites ? favorites : list;
     return (
