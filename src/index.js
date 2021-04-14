fe68a2830da843e63732203a4ba00110c4220a90
Arjun Khanduri <arjun99.khanduri@gmail.com>
2021-04-15 00:57:11 +0530

    Added middleware

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import './index.css';
 import App from './components/App';
 import rootReducer from './reducers';
 
-const store = createStore(rootReducer);
+const logger = function ({ dispatch, getState }) {
+    return function (next) {
+        return function (action) {
+            console.log('ACTION_TYPE = ', action.type);
+            next(action);
+        }
+    }
+}
+
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 ReactDOM.render(<App store={store} />, document.getElementById('root'));
 
